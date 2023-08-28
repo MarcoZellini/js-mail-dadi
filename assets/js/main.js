6125c712e1b47e05e14150fec4cdb6ed5b2c587f
Marco Zellini <zellinimarco@gmail.com>
2023-08-28 16:49:38 +0200

    Add email check

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,10 +21,10 @@ const userResultElement = document.querySelector('#user_result');
 const computerResultElement = document.querySelector('#computer_result');
 
 //Scrivo i risultati sui vari nodi
-userResultElement.append("Il numero dell'utente è: " + userNumber );
-computerResultElement.append("Il numero del computer è: " + computerNumber );
+userResultElement.append("Il numero dell'utente è: " + userNumber);
+computerResultElement.append("Il numero del computer è: " + computerNumber);
 
-if(userNumber > computerNumber) {
+if (userNumber > computerNumber) {
     console.log("Ha vinto l'utente!");
     diceResultElement.append("Ha vinto l'utente!");
 } else if (userNumber < computerNumber) {
@@ -35,4 +35,47 @@ if(userNumber > computerNumber) {
     diceResultElement.append("Pari merito!");
 }
 
+/* MAIL */
 
+//creo la mia lista di email
+const emailList = [];
+
+//assegno vari valori alla mia lista
+emailList.push('abc@gmail.com');
+emailList.push('ciao@libero.it');
+emailList.push('zellini@hotmail.com');
+emailList.push('marco@outlook.com');
+emailList.push('pippo@yahoo.com');
+emailList.push('hello@gmail.com');
+
+//definisco una costante che conterrà un elemento bottone
+const buttonElement = document.querySelector("button[type='submit']");
+console.log(buttonElement);
+
+//al click del bottone scatenerò un evento
+buttonElement.addEventListener('click', function (e) {
+    //prevengo il comportamento di default del buttone submit, perciò prevengo il refresh della pagina
+    e.preventDefault();
+
+    //definisco le mie variabili
+    const emailInputElement = document.querySelector("input[type='email']");
+    const emailInputValue = emailInputElement.value;
+    let emailCheckMessage;
+
+    console.log("emailInputElement, emailInputValue -->", emailInputElement, emailInputValue);
+
+    //controllo se la mail inserita si trova all'interno nella lista
+    if (emailList.includes(emailInputValue)) {
+        emailCheckMessage = `Benvenuto ${emailInputValue}! Puoi accedere a questo sito!`;
+        console.log(`Benvenuto ${emailInputValue}! Puoi accedere a questo sito!`);
+    } else {
+        emailCheckMessage = `Attenzione! L'indirizzo mail ${emailInputValue} non esiste!`;
+        console.log(`Attenzione! L'indirizzo mail inserito non esiste!`);
+    }
+
+    //scrivo in pagina il risultato della ricerca
+    const emailCheckResultElement = document.getElementById('email_check_result');
+    console.log(emailCheckResultElement);
+    emailCheckResultElement.innerHTML = " ";
+    emailCheckResultElement.append(emailCheckMessage);
+})
